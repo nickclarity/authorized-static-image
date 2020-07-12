ad2e2f1c6a2f3644fd8d607b6d13f5d6546165a3
Nick Clarity <nick_clarity@hotmail.com>
2020-07-12 16:49:03 -0500

    initial commit

---
+++ Identity/Controllers/ImagesController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.IO;
+
+namespace Identity.Controllers
+{
+    public class ImagesController : Controller
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public ImagesController(IWebHostEnvironment env)
+        {
+            
+            _env = env;
+        }
+
+        //[Authorize]
+        [HttpGet]
+        [Route("images/{fileName}.{ext?}")]
+        public IActionResult GetImage(string fileName, string ext)
+        {
+            if (ext == null)
+                return NotFound();
+
+            var user = HttpContext.User.Identity.Name;
+            string clientPath;
+
+            // TODO: Tennat ID
+            switch(user) {
+                case "user@clienta.com":
+                    clientPath = "ClientA";
+                    break;
+                case "user@clientb.com":
+                    clientPath = "ClientB";
+                    break;
+                default:
+                    clientPath = "default";
+                    break;
+            }
+
+            var filePath = Path.Combine(
+                _env.ContentRootPath, "BrandedContent", clientPath, "images", $"{fileName}.{ext}");
+
+            PhysicalFileResult file;
+
+            try
+            {
+                file = PhysicalFile(filePath, "image/jpeg");
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+
+            //const int durationInSeconds = 60;
+            //Response.Headers[HeaderNames.CacheControl] =
+            //    "public,max-age=" + durationInSeconds;
+
+            Response.Headers[HeaderNames.CacheControl] =
+                "no-store,no-cache";
+
+            return file;
+        }
+    }
+}
---
+++ Identity/Startup.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Identity.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+
+namespace Identity
+{
+    public class Startup
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            _env = env;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddRazorPages();
+            services.AddControllersWithViews();
+            services.AddSingleton(_env);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
